772d5aed82dc6d64ac7326f2fac0bc41b5b35ba8
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-01-31 08:52:52 +0300

    adding images

--- src/index.js
+++ src/index.js
@@ -26,14 +26,26 @@ const Book = () => {
 
 const Image = () => (
   <img
-    src="https://images-na.ssl-images-amazon.com/images/I/91jbHTNpy6L._AC_UL900_SR900,600_.jpg"
+    // src="https://images-na.ssl-images-amazon.com/images/I/91jbHTNpy6L._AC_UL900_SR900,600_.jpg"
+    // alt="House of Flame and shadows"
+    src="./images/flame.jpg"
     alt="House of Flame and shadows"
   />
 );
 
 const Title = () => <h2>House of Flame and shadows</h2>;
 const Author = () => {
-  return <h4>Sarah J</h4>;
+  const inlineHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+  return (
+    // <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
+    //   Sarah J
+    // </h4>
+    <h4 style={inlineHeadingStyles}>Sarah J</h4>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
