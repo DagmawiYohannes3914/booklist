a23c2578355e035a35fae0d2b53f7e360f4a5d34
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-02-01 15:22:19 +0300

    Event handler

--- src/index.js
+++ src/index.js
@@ -3,43 +3,62 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const firstBook = {
-  author: "Sarah J",
-  title: "House of Flame and shadows",
-  img: "./images/flame.jpg",
-};
-const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg",
-};
+const books = [
+  {
+    author: "Sarah J",
+    title: "House of Flame and shadows",
+    img: "./images/flame.jpg",
+    id: "1",
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg",
+    id: "2",
+  },
+];
 
-// const author = "Sarah J";
-// const title = "House of Flame and shadows";
-// const img = "./images/flame.jpg";
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      <EventExamples />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
+};
+
+const EventExamples = () => {
+  const handleformInput = () => {
+    console.log("handle form input");
+  };
+  const handlebuttonclick = () => {
+    alert("handle form button");
+  };
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleformInput}
+          style={{ margin: "1rem 0" }}
+        />
+      </form>
+      <button onClick={handlebuttonclick}>click me</button>
     </section>
   );
 };
 
 const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
@@ -47,27 +66,3 @@ const Book = (props) => {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
-
-// const Image = () => (
-//   <img
-//     // src="https://images-na.ssl-images-amazon.com/images/I/91jbHTNpy6L._AC_UL900_SR900,600_.jpg"
-//     // alt="House of Flame and shadows"
-//     src="./images/flame.jpg"
-//     alt="House of Flame and shadows"
-//   />
-// );
-
-// const Title = () => <h2>House of Flame and shadows</h2>;
-// const Author = () => {
-//   const inlineHeadingStyles = {
-//     color: "#617d98",
-//     fontSize: "0.75rem",
-//     marginTop: "0.5rem",
-//   };
-//   return (
-//     // <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
-//     //   Sarah J
-//     // </h4>
-//     // <h4 style={inlineHeadingStyles}>Sarah J</h4>
-//   );
-// };
