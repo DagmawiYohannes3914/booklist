d5157b4cfccca00fa603a764b6ea7bfe2a7ea0c3
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-02-02 15:39:24 +0300

    components update

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ const books = [
 const BookList = () => {
   return (
     <section className="booklist">
-      <EventExamples />
       {books.map((book) => {
         return <Book {...book} key={book.id} />;
       })}
@@ -30,46 +29,18 @@ const BookList = () => {
   );
 };
 
-const EventExamples = () => {
-  const handleformInput = (e) => {
-    console.log("handle form input");
-    console.log(e.target);
-    console.log(e.target.name);
-    console.log(e.target.value);
-  };
-  const handlebuttonclick = () => {
-    alert("handle form button");
-  };
-  const handleFormSubmission = (e) => {
-    e.preventDefault();
-    console.log("handle form submission");
-  };
-  return (
-    <section>
-      <form onSubmit={handleFormSubmission}>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={handleformInput}
-          style={{ margin: "1rem 0" }}
-        />
-        <button type="submit">Submit</button>
-        {/* <button type="submit" onClick={handleFormSubmission}>Submit</button> */}
-        <div>
-          <button onClick={handlebuttonclick}>click me</button>
-        </div>
-      </form>
-    </section>
-  );
-};
-
 const Book = (props) => {
   const { img, title, author } = props;
+
+  const displayTitle = () => {
+    console.log(title);
+  };
+
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={displayTitle}>Display Title</button>
       <h4>{author}</h4>
     </article>
   );
