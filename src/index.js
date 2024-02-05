f10bec714c18b517f7c2381a79ba1ce6e835be07
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-02-05 15:21:43 +0300

    get book function

--- src/index.js
+++ src/index.js
@@ -20,31 +20,34 @@ const books = [
 ];
 
 const BookList = () => {
-  const SomeValue = "shakeAndBake";
-  const displayValue = () => {
-    console.log(SomeValue);
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
   };
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} displayValue={displayValue} />;
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author, displayValue } = props;
+  const { img, title, author, getBook, id } = props;
+  // console.log(props);
 
   //   const displayTitle = () => {
   //     console.log(title);
   //   };
-
+  const getSingleBook = () => {
+    getBook(id);
+  };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayValue}>Click me</button>
+      <button onClick={getSingleBook}>Click me</button>
       <h4>{author}</h4>
     </article>
   );
