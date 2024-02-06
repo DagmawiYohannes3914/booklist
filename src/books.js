35ad8da9c3a1609a32e4b5e637a64e940d71566b
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-02-06 09:42:26 +0300

    code optimization

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/atomic.jpg";
+import img2 from "./images/flame.jpg";
+import img3 from "./images/fourthwing.jpg";
+
+export const books = [
+  {
+    author: "Sarah J",
+    title: "House of Flame and shadows",
+    img: img1,
+    id: "1",
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img2,
+    id: "2",
+  },
+  {
+    author: "Rebecca Yarros",
+    title: "Fourth Wing",
+    img: img3,
+    id: "3",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,54 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-// import { hasFormSubmit } from "@testing-library/user-event/dist/utils";
-
-const books = [
-  {
-    author: "Sarah J",
-    title: "House of Flame and shadows",
-    img: "./images/flame.jpg",
-    id: "1",
-  },
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "./images/atomic.jpg",
-    id: "2",
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author, getBook, id } = props;
-  // console.log(props);
-
-  //   const displayTitle = () => {
-  //     console.log(title);
-  //   };
-  const getSingleBook = () => {
-    getBook(id);
-  };
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={getSingleBook}>Click me</button>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
